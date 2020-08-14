a4bb3657d2d0d06ca31c57784bf4146f2d81ab01
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-08-14 17:55:36 -0500

    Added more to PokemonList.js

--- pokedex/src/components/pokemon/PokemonList.js
+++ pokedex/src/components/pokemon/PokemonList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
 import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
+state= {
+    url: "https://pokeapi.co/api/v2/pokemon",
+    pokemon: null
+};
+
+componentDidMount() {
+    const res = axios.get(this.state.url);
+    this.setState({pokemon: res.data['results']});
+}
+
     render() {
         return (
-            <div>
+            <div className="row">
+                <PokemonCard />
+                <PokemonCard />
+                <PokemonCard />
+                <PokemonCard />
+                <PokemonCard />
                 <PokemonCard />
             </div>
         )
