4bcc660cd51cc92a6ff03cc762071ac595d37b53
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-08-14 18:41:03 -0500

    Added to PokemonList.js

--- pokedex/src/components/pokemon/PokemonList.js
+++ pokedex/src/components/pokemon/PokemonList.js
@@ -9,21 +9,24 @@ state= {
     pokemon: null
 };
 
-componentDidMount() {
-    const res = axios.get(this.state.url);
+async componentDidMount() {
+    const res = await axios.get(this.state.url);
     this.setState({pokemon: res.data['results']});
 }
 
     render() {
         return (
-            <div className="row">
-                <PokemonCard />
-                <PokemonCard />
-                <PokemonCard />
-                <PokemonCard />
-                <PokemonCard />
-                <PokemonCard />
-            </div>
-        )
+        <React.Fragment>
+            {this.state.pokemon ? (
+                <div className="row">
+                    {this.state.pokemon.map(pokemon => (
+                        <PokemonCard/>
+                    ))}
+                </div>
+            ) : (
+                <h1>Loading Pokemon</h1>
+            )}
+        </React.Fragment>
+        );
     }
 }
