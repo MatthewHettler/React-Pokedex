cdd2222f8e0c466d8ce448de955b58c0bdfcf7a5
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-08-21 16:33:14 -0500

    Created background and added styling.

--- pokedex/src/components/pokemon/PokemonCard.js
+++ pokedex/src/components/pokemon/PokemonCard.js
@@ -10,6 +10,18 @@ const Sprite = styled.img`
     display: none
 `;
 
+const Card = styled.div`
+box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+&:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)
+}
+-moz-user-select: none;
+-website-user-select: none;
+user-select: none;
+-o-user-select: none;
+`;
+
 export default class PokemonCard extends Component {
 state = {
     name: '',
@@ -35,7 +47,7 @@ componentDidMount () {
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
+                <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
                     {this.state.imageLoading ? (
                         <img src={spinner} style={{width: '5em', height: '5em'}} className="card-img-top rounded mx-auto d-block mt-2"/>
@@ -46,8 +58,8 @@ componentDidMount () {
                     onError={() => this.setState({toManyRequests: true})}
                     src={this.state.imageUrl}
                     style={
-                        this.state.toManyRequests ? { display: "none"} :
-                        this.state.imageLoading ? null : { display: "block"}
+                        this.state.toManyRequests ? { display: 'none'} :
+                        this.state.imageLoading ? null : { display: 'block'}
                     }
                     />
                     {this.state.toManyRequests ? (
@@ -58,7 +70,7 @@ componentDidMount () {
                     <div className="card-body mx-auto">
                     <h6 className="card-title">{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(' ')}</h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
