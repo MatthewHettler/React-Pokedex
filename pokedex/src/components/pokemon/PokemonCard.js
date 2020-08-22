3b3576c7df18cefa019cc6d5fecaf27129e16148
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-08-22 17:12:14 -0500

    Incorporated different states into the app.

--- pokedex/src/components/pokemon/PokemonCard.js
+++ pokedex/src/components/pokemon/PokemonCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 import styled from 'styled-components';
 
@@ -20,6 +21,19 @@ transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 -website-user-select: none;
 user-select: none;
 -o-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+
+text-decoration: none;
+color: black;
+&:focus,
+&:hover,
+&:visited,
+&:link,
+&:active {
+    text-decoration: none;
+}
 `;
 
 export default class PokemonCard extends Component {
@@ -47,6 +61,7 @@ componentDidMount () {
     render() {
         return (
             <div className="col-md-3 col-sm-6 mb-5">
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
                 <Card className="card">
                     <h5 className="card-header">{this.state.pokemonIndex}</h5>
                     {this.state.imageLoading ? (
@@ -71,6 +86,7 @@ componentDidMount () {
                     <h6 className="card-title">{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase() + letter.substring(1)).join(' ')}</h6>
                     </div>
                 </Card>
+                </StyledLink>
             </div>
         )
     }
