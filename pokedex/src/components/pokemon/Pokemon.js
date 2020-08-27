422f50e94d7a87e72ba04f4da118fc63558355d6
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-08-27 17:50:59 -0500

    Add final touches to the info for each pokemon.

--- pokedex/src/components/pokemon/Pokemon.js
+++ pokedex/src/components/pokemon/Pokemon.js
@@ -107,11 +107,11 @@ export default class componentName extends Component {
             }
             return false;
         }).map(stat => {
-            return `${stat.effort} ${stat.stat.name}`
+            return `${stat.effort} ${stat.stat.name
             .toLowerCase()
             .split('-')
             .map(s => s.charAt(0).toUpperCase() + s.substring(1))
-            .join(' ');
+            .join(' ')}`;
         })
         .join(',');
 
@@ -341,9 +341,93 @@ export default class componentName extends Component {
                                         <h6 className="float-left">{this.state.height} ft.</h6>
                                     </div>
                                 </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                    <h6 className="float-right">Weight:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6 className="float-left">{this.state.weight} lbs.</h6>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                    <h6 className="float-right">Catch Rate:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6 className="float-left">{this.state.catchRate}%</h6>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                    <h6 className="float-right">Gender Ratio:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="progress">
+                                            <div className="progress-bar" role="progressbar" style={{
+                                                width: `${this.state.genderRatioFemale}%`,
+                                                backgroundColor: '#C2185B'
+                                            }}
+                                            aria-valuenow="15"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                            >
+                                                <small>{this.state.genderRatioFemale}</small>
+                                            </div>
+                                            <div className="progress-bar" role="progressbar" style={{
+                                                width: `${this.state.genderRatioMale}%`,
+                                                backgroundColor: '#1976D2'
+                                            }}
+                                            aria-valuenow="30"
+                                            aria-valuemin="0"
+                                            aria-valuemax="100"
+                                            >
+                                                <small>{this.state.genderRatioMale}</small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+
+                            <div className="col-md-6">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <h6 className="float-right">Egg Groups:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6 className="float-left">{this.state.eggGroups}</h6>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <h6 className="float-right">Hatch Steps:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6 className="float-left">{this.state.hatchSteps}</h6>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <h6 className="float-right">Abilities:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6 className="float-left">{this.state.abilities}</h6>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <h6 className="float-right">EVs:</h6>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <h6 className="float-left">{this.state.evs}</h6>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="card-footer text-muted">
+                            Data From <a href="https://pokeapi.co/" target="_blank" className="card-link">PokeAPI.co</a>
+                        </div>
                 </div>
             </div>
         );
